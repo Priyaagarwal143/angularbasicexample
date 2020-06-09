4e1e6d58172377c1f05f09c321c290c3e3fe428e
Priyaagarwal143 <agarwalpriya303@gmail.com>
2020-06-09 23:00:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FelixProject';
+  
+  name = 'Priya';
+  email = 'agarwalpriya303@gmail.com';
+  gender = 'Female';
+  age = 26;
+  contact = 7038738846;
+
+  public a = 6;
+  public b = 48;
+
+  objValues = {
+    key1 : 'Goyal',
+    key2 : 4763524,
+    key3 : 'Sagar'
+  };
+
+  arrayValues = [20,36,17,'Priya','Sahil',78];
+
+  showAlert(){
+    alert('Hello from Angular');
+  }
+
+  showCalculation(a,b){
+    alert('Sum of a + b : ' + (a + b));
+  }
+
+  showCalculation1(c,d){
+    document.getElementById('demo').innerHTML = c + d;
+  }
+
+  getValue(i){
+    console.warn(i.target.value);
+  }
+
+  getValue1(i){
+    console.warn(i);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+import {MatInputModule} from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainheaderComponent } from './header/mainheader/mainheader.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeadComponent } from './head/head.component';
+import { FootComponent } from './foot/foot.component';
+import { AsideComponent } from './aside/aside.component';
+import { SectionComponent } from './section/section.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainheaderComponent,
+    FooterComponent,
+    HeadComponent,
+    FootComponent,
+    AsideComponent,
+    SectionComponent,
+    ArticleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
